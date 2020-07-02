986007d312ecd565916f0352b776a6eb5dc03cfb
Logan Miner <loganrobertminer@gmail.com>
2020-07-02 09:30:07 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  Button,
+  // Button,
   Navbar,
   Nav,
-  NavDropdown,
-  Form,
-  FormControl,
+  // NavDropdown,
+  // Form,
+  // FormControl,
+  Carousel,
 } from "react-bootstrap";
 function App() {
   return (
-    <div className="App container">
+    <div className="App container-fluid">
       <div className="row">
         <div className="col p-0">
           <Navbar bg="dark" expand="lg" variant="dark">
@@ -28,12 +29,62 @@ function App() {
         </div>
       </div>
       <div className="row">
-        <div className="col">B</div>
+        <div className="col p-0"><ControlledCarousel></ControlledCarousel></div>
       </div>
-      <div className="row">
+      {/* <div className="row">
         <div className="col">C</div>
-      </div>
+      </div> */}
     </div>
+  );
+}
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/200"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/200"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/200"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
