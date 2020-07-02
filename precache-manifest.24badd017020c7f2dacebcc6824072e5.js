d032f9426f21a5ead00cd790e189cbf530c52098
Logan Miner <loganrobertminer@gmail.com>
2020-07-02 11:47:55 -0400

    Updates

---
+++ precache-manifest.24badd017020c7f2dacebcc6824072e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7d5c44eba577065a58d8d83cd3a9dd",
+    "url": "/caledonia-tile-and-flooring-co/index.html"
+  },
+  {
+    "revision": "c83b33ded2d833a751eb",
+    "url": "/caledonia-tile-and-flooring-co/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3ccb3f3f9aaebfd26f2e",
+    "url": "/caledonia-tile-and-flooring-co/static/js/2.64640f14.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/caledonia-tile-and-flooring-co/static/js/2.64640f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83b33ded2d833a751eb",
+    "url": "/caledonia-tile-and-flooring-co/static/js/main.0745e369.chunk.js"
+  },
+  {
+    "revision": "4d153f79720ab36413a2",
+    "url": "/caledonia-tile-and-flooring-co/static/js/runtime-main.4648d789.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/caledonia-tile-and-flooring-co/precache-manifest.24badd017020c7f2dacebcc6824072e5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/caledonia-tile-and-flooring-co/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
