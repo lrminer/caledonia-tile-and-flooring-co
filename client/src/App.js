0d0e27ed666ba1d66a89911b76b2f8d71c3459d5
Logan Miner <loganrobertminer@gmail.com>
2020-07-02 09:23:05 -0400

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {
+  Button,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+} from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className="row">
+        <div className="col p-0">
+          <Navbar bg="dark" expand="lg" variant="dark">
+            <Navbar.Brand href="#home">
+              Caledonia Tile & Flooring Co.
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">About</Nav.Link>
+                <Nav.Link href="#link">Portfolio</Nav.Link>
+                <Nav.Link href="#link">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">B</div>
+      </div>
+      <div className="row">
+        <div className="col">C</div>
+      </div>
     </div>
   );
 }
